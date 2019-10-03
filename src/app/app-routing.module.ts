0ef45259a84ca19fc25643cbf6d711d4c94a2bc0
Claudia Tienda <claudiaivontienda@hotmail.com>
2019-10-03 02:48:33 -0500

    menus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuInicialComponent } from './menu-inicial/menu-inicial.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'menu', component: MenuInicialComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuInicialComponent } from './menu-inicial/menu-inicial.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuInicialComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
