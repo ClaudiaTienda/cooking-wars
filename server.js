56b5fc89718e08b791b6f8855e8619a511d86122
Claudia Tienda <claudiaivontienda@hotmail.com>
2019-10-04 05:03:24 -0500

    links

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cooking-wars'));
+app.use(express.static(__dirname + '/dist/cooking-wars/'));
 
 app.get('/*', function(req,res) {
     
