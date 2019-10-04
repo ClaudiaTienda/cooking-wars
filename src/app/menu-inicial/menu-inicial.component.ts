631ea7435231bca9871b50af1512effdec8fdfa7
Claudia Tienda <claudiaivontienda@hotmail.com>
2019-10-04 04:32:26 -0500

    links

--- src/app/menu-inicial/menu-inicial.component.ts
+++ src/app/menu-inicial/menu-inicial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-menu-inicial',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuInicialComponent implements OnInit {
 
+  router: Router;
   constructor() { }
 
   ngOnInit() {
   }
 
+  menu(): void {
+    this.router.navigate(['/opciones']);
+  }
+  puntuaciones(): void {
+    this.router.navigate(['/puntuaciones']);
+  }
+  inicio(): void {
+    this.router.navigate(['/inicio']);
+  }
 }
